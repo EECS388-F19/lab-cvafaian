c6b89e3e4066a335c60976ced2b018a19b70ff0f
cvafaian <cvafaian@umich.edu>
2019-09-09 15:47:31 -0400

    Initial Commit

---
+++ helloworld.py
@@ -0,0 +1,15 @@
+
+from random import randint
+a = randint (1,100)
+b = randint (1,100)
+
+result = a + b
+
+print ("Christina Vafaian")
+print ("a")
+print ("b")
+print ("Sum = " + result )
+print ("Average =" + result/2)
+
+
+
