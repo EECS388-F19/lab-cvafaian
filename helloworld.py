a0f50ce6440042fdc5010725aac91bd8ced64794
cvafaian <cvafaian@umich.edu>
2019-09-09 16:27:11 -0400

    Added second excercise

--- helloworld.py
+++ helloworld.py
@@ -8,8 +8,22 @@ result = a + b
 print ("Christina Vafaian")
 print ("a")
 print ("b")
-print ("Sum = " + result )
-print ("Average =" + result/2)
+print ("Sum = " + str(result))
+print ("Average =" + str(result/2.0))
 
 
+
+student = ["Joul" , "Ankit" , "Alex"]
+student.sort()
+print (student)
+
+first_name = student[0]
+first_name = first_name[:-1]
+print first_name
+
+longest_name = ''
+for student in students:
+    if len(student) > len(longest_name):
+        longest_name = student
+print(longest_name)
 
